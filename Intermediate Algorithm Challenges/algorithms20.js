32396a61051320add7b3cab37b57de750f0d2c1a
Jaldhar H. Vyas <jaldhar@braincells.com>
2017-04-10 17:11:27 -0400

    Intermediate algorithm challenge 20: Everything Be True

---
+++ Intermediate Algorithm Challenges/algorithms20.js
@@ -0,0 +1,14 @@
+// Everything Be True
+
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  for (var i = 0; i < collection.length; i++) {
+    if (!collection[i].hasOwnProperty(pre) || !collection[i][pre]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
